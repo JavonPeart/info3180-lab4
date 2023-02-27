305cd57ce519ae9ee8a3791909e1f5ec6c7b145a
JavonPeart <44687372+JavonPeart@users.noreply.github.com>
2023-02-26 19:13:45 -0500

    Completed Exercise 3 and 4

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -20,10 +21,10 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Javon Peart")
 
 
-@app.route('/upload', methods=['POST', 'GET'])
+@app.route('/upload/', methods=['POST', 'GET'])
 def upload():
     # Instantiate your form class
 
@@ -43,20 +44,27 @@ def login():
 
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
+    if form.validate_on_submit():
         # Get the username and password values from the form.
+        username = form.username.data
+        password = form.password.data
 
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
         # Then store the result of that query to a `user` variable so it can be
         # passed to the login_user() method below.
+        user = db.session.execute(db.select(UserProfile).filter_by(username=username)).scalar()
+        
+        if user is not None and check_password_hash(user.password, password):
+            # Gets user id, load into session
+            login_user(user)
+            flash('Logged in Successfully', 'success')
 
-        # Gets user id, load into session
-        login_user(user)
-
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
+            # Remember to flash a message to the user
+            return redirect(url_for("upload"))  # The user should be redirected to the upload form instead
+        else:
+            flash('Incorrect Username/Password','failure')
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
